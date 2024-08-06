0885cc47c9f086a4bcac762827f3dfac75a0c9c8
Timothy Macfarlane <timdiscovers@gmail.com>
2024-08-06 15:47:56 -0500

    new project structure and dependencies

--- AskCodeium/services/codeium_service.py
+++
@@ -1,7 +0,0 @@
-class CodeiumService:
-    @staticmethod
-    async def send_request(chat_thread, query):
-        context = chat_thread.get_context()
-        # Simulate sending a request to Codeium and getting a response
-        response = f"Response based on context: {context} and query: {query}"
-        return response
