da0bf6932379254096a45b338586c2abb4b05d95
Timothy Macfarlane <timdiscovers@gmail.com>
2024-08-07 10:00:28 -0500

    cleared tests

--- testing.py
+++ testing.py
@@ -1,10 +1,3 @@
 from AskCodeium import createChat
 
 chat1 = createChat()
-
-print(chat1("What is Python?"))
-print(chat1("What is JavaScript?"))
-print("Chat1 History:", chat1.get_history())
-chat1.clear_history()
-print("Chat1 History:", chat1.get_history())
-chat1.close()
